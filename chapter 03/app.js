5241b9374b6d8a577dd9a6229f124a4232f2c529
haroon124 <agar3d@gmail.com>
2023-09-09 16:02:33 +0500

    chapter 03

---
+++ chapter 03/app.js
@@ -0,0 +1,18 @@
+// question 01
+let age = 17
+alert(age)
+
+// question 02
+let visitingNumber = 0
+visitingNumber = 1
+alert("You have visited this site " + visitingNumber + " times")
+
+// question 03
+let birthYear = 2006
+document.write("My birth-year is " + birthYear)
+
+// question 04
+let visitorName = "Kamil"
+let product = "pent"
+let productOrder = 10
+document.write("\nMr " + visitorName + " ordered " + productOrder + product + " on XYZ-clothing-store.")
