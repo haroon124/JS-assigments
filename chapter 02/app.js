35c3c0e87c4624ef256bf561b500acf1dfb4364e
haroon124 <agar3d@gmail.com>
2023-09-09 15:50:48 +0500

    chapter 02

---
+++ chapter 02/app.js
@@ -0,0 +1,37 @@
+// question 01
+var userName = "Muhammad Haroon"
+
+// question 02
+let myName = "Muhammad Haroon"
+
+// question 03
+const message = "Hello World!"
+alert(message)
+
+// question 04
+const studentName = "Sami"
+const studentAge = "22"
+alert(studentName)
+alert(studentAge)
+
+// question 05
+let pizza = " pizza \n pizz \n piz \n pi \n p"
+
+// question 06
+const email = "haroon.aghani@gmail.com"
+alert("My email address is" + email)
+
+// question 07
+const book = "A smarter way to learn JavaScript"
+alert(book)
+
+// question 08
+var text = ("Yah! I can write HTML content through JavaScript.");
+document.write(text)
+
+// question 09
+let design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(design)
+
+
+
