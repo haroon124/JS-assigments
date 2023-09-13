bb82fe741e7fbb91c2680fb1ce2d1469b287c2f9
haroon124 <agar3d@gmail.com>
2023-09-13 21:56:15 +0500

    chapter 17 to 20

---
+++ chapter 17 to 20/app.js
@@ -0,0 +1,33 @@
+// question 01
+
+let q1 = [[],[]]
+
+// question 02
+
+let q2 = [[0,1,2,3 + "<br>"] +[1,0,1,2 + "<br>"] + [2,1,0,1 + "<br><br><br><br>"]]
+document.write(q2)
+
+// question 03
+
+for(var i = 0; i <= 10; i++){
+    document.write(i + "<br>")
+}
+
+// question 04
+
+let tableNumber = +prompt ("which table you want to see?")
+let tableLength = +prompt ("write a length of table.")
+document.write("<br><br><br><br>")
+
+for(var i = 1; i<= tableLength; i++) {
+    document.write("" + tableNumber + " x " + i + " = " + (tableNumber*i) + "<br>")
+}
+
+// question 05
+
+// let fruits = ["apple", "banana","mango" , "orange","strawberry"]
+
+// for(var i = 0; i <SVGDefsElement fruits.length; i++) {
+//     document.write("element at index" )
+// }
+
